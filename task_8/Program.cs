c3129fe356400ae7272b1cbbc41f01a7a9b17f58
Igor Chichinin <marafon4k@gmail.com>
2022-07-09 14:59:19 +0300

    task_8 added solutions and additional sol.

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -4,11 +4,35 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Введите число: ");
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int i = 0;
+int i = 1;
 
-while (i > -number && i < number) {
-    Console.WriteLine(-number + i);
-    i++;
+while (i < number) {
+    int test = i % 2;
+    if (test == 0) {
+        Console.WriteLine(i);
+        i++;
+    } else {
+        i++;
+    }
 }
+
+// сначала неправильно прочитал задачу, начал выполнять, потом выполненные решения оставил как
+// доп. задача
+
+
+// Пользователь задает число N, мы выводим все числа от -N до 0
+
+// for (int i = 0; i < number; i++) Console.WriteLine(-number + i); // пробный вывод 1
+
+
+// пробный код 2
+
+// while (i >= -number && i <= number) {
+//     Console.WriteLine(-number + i);
+//     i++;
+//     if (i > -number) {
+//         Console.WriteLine(i);
+//     }
+// }
