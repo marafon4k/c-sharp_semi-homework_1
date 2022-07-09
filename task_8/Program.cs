d391a60bc3eb6f20888e0c01edcf53f6971fb8c3
Igor Chichinin <marafon4k@gmail.com>
2022-07-09 14:23:35 +0300

    task_8 added half solutions

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -4,3 +4,11 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i > -number && i < number) {
+    Console.WriteLine(-number + i);
+    i++;
+}
