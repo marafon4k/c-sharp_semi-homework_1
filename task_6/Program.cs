f2ed29418ae04550856eb68fa58b955f7f5ce7af
Igor Chichinin <marafon4k@gmail.com>
2022-07-09 14:14:59 +0300

    task_6 added solution

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -4,3 +4,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a % 2;
+// Console.WriteLine(result);
+
+if (result == 0) {
+    Console.WriteLine("Да, число четное");
+} else {
+    Console.WriteLine("Нет, число нечетное");
+}
